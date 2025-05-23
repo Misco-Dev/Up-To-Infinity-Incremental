97e9c2edfedcad64f1d485dbee7e7c441624ba54
misco <miscojohnes1@gmail.com>
2025-05-24 00:06:14 +0200

    Bugfix: Fix shop page layout issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import image from '@astrojs/image';
 export default defineConfig({
     site: 'http://uptoinfinityincremental.work.gd',
+    integrations: [image({
+        serviceEntryPoint: '@astrojs/image/static',
+    })],
     image: {
         service: {
             entrypoint: "astro/assets/services/static",
