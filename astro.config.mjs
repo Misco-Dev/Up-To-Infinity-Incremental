dcd55e851d64664af1c9060e478a3ddd7390a0bd
misco <miscojohnes1@gmail.com>
2025-05-24 00:15:32 +0200

    Bugfix: Fix shop page layout issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
+
 export default defineConfig({
     site: 'http://uptoinfinityincremental.work.gd',
     integrations: [image({
