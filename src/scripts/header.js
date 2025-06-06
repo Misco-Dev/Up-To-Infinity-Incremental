878d3287ba6b5e4af0b43c4db73c8c9a2b02d5c4
misco <miscojohnes1@gmail.com>
2025-06-06 22:18:50 +0200

    Refactor countdown and header scripts to use external JavaScript files

---
+++ src/scripts/header.js
@@ -0,0 +1,11 @@
+document.addEventListener('click', function (event) {
+    const toggle = document.getElementById('menu-toggle');
+    const icon = document.querySelector('.menu-icon');
+    const links = document.querySelector('.nav-links');
+
+    const isClickInside = toggle.contains(event.target) || icon.contains(event.target) || links.contains(event.target);
+
+    if (!isClickInside && toggle.checked) {
+    toggle.checked = false;
+    }
+});
