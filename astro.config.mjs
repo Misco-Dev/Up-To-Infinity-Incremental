fadc63e4bb1bc6073e0a734873232b066e7001a3
misco <miscojohnes1@gmail.com>
2025-06-11 15:25:25 +0200

    Update Astro configuration site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-    site: 'http://uptoinfinityincremental.work.gd',
+    site: 'https://qronexstudios.com/',
     adapter: vercel({
 		webAnalytics: {
 			enabled: true,
