92bd21df94e56836fbe2b33a84dd9751ee9a8cad
misco <miscojohnes1@gmail.com>
2025-06-12 23:11:32 +0200

    Upgraded astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
     site: 'https://qronexstudios.com/',
     adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-  	}),
-	defaultLocale: "en",
-});
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
+    defaultLocale: 'en',
+})
