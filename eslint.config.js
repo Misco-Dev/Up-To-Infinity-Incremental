6e3658530778468cb71a92e3e237c0ef3362990e
misco <miscojohnes1@gmail.com>
2025-06-12 23:08:47 +0200

    Fixed esLint bugs

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,63 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import astro from "eslint-plugin-astro";
-import astroParser from "astro-eslint-parser";
-import { defineConfig } from "eslint/config";
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import astro from 'eslint-plugin-astro'
+import astroParser from 'astro-eslint-parser'
 
-export default defineConfig([
-	{
-		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-		languageOptions: {
-			globals: globals.browser,
-			parser: tseslint.parser,
-			parserOptions: {
-				sourceType: "module",
-			},
-		},
-		extends: ["eslint:recommended", ...tseslint.configs.recommended.extends],
-	},
-	{
-		files: ["**/*.astro"],
-		plugins: { astro },
-		languageOptions: {
-			parser: astroParser,
-			parserOptions: {
-				parser: tseslint.parser,
-				extraFileExtensions: [".astro"],
-				sourceType: "module",
-			},
-			globals: globals.browser,
-		},
-		processor: astro.processors[".astro"],
-		rules: {
-			"no-undef": "off",
-			"no-unused-vars": "off",
-		},
-	},
-	{
-		"extends": [
-			"some-other-configs",
-			"plugin:prettier/recommended"
-		]
-	},
-	globalIgnores(['.vercel/']),
-]);
+export default [
+    // Ignore patterns
+    {
+        ignores: [
+            'dist/**/*',
+            '.astro/**/*',
+            '.vercel/**/*',
+            'node_modules/**/*'
+        ]
+    },
+    // TypeScript and JavaScript files
+    ...tseslint.configs.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.es2025
+            },
+            parser: tseslint.parser,
+            parserOptions: {
+                sourceType: 'module',
+                ecmaVersion: 'latest',
+                ecmaFeatures: {
+                    jsx: true
+                }
+            },
+        },
+        rules: {
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+            'no-unused-vars': 'warn'
+        }
+    },
+    {
+        files: ['**/*.astro'],
+        plugins: { astro },
+        languageOptions: {
+            parser: astroParser,
+            parserOptions: {
+                parser: tseslint.parser,
+                extraFileExtensions: ['.astro'],
+                sourceType: 'module',
+                ecmaVersion: 'latest'
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.es2025
+            }
+        },
+        processor: astro.processors['.astro'],
+        rules: {
+            'no-undef': 'off',
+            'no-unused-vars': 'warn',
+            'astro/no-conflict-set-directives': 'error',
+            'astro/no-unused-define-vars-in-style': 'error'
+        }
+    }
+]
