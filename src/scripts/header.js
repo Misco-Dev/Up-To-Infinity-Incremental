0006115853a5606e68157512afa6a4ab54e5502d
misco <miscojohnes1@gmail.com>
2025-06-12 23:16:10 +0200

    Upgraded header.js

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,11 +1,12 @@
 document.addEventListener('click', function (event) {
-    const toggle = document.getElementById('menu-toggle');
-    const icon = document.querySelector('.menu-icon');
-    const links = document.querySelector('.nav-links');
+    const toggle = document.getElementById('menu-toggle')
+    const icon = document.querySelector('.menu-icon')
+    const links = document.querySelector('.nav-links')
 
-    const isClickInside = toggle.contains(event.target) || icon.contains(event.target) || links.contains(event.target);
+    const isClickInside =
+        toggle.contains(event.target) || icon.contains(event.target) || links.contains(event.target)
 
     if (!isClickInside && toggle.checked) {
-    toggle.checked = false;
+        toggle.checked = false
     }
-});
+})
