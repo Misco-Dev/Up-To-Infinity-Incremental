e8450f05f68acadc0ac1fe0ae971ed4eff0ececf
misco <miscojohnes1@gmail.com>
2025-06-15 09:14:28 +0200

    Add PWA integration and service worker registration for improved performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel'
+import { VitePWA } from '@vite-pwa/astro';
 
 export default defineConfig({
     site: 'https://qronexstudios.com/',
@@ -9,4 +10,25 @@ export default defineConfig({
         },
     }),
     defaultLocale: 'en',
+    integrations: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Up To Infinity Incremental',
+                start_url: '/',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: '/image/icon.ico',
+                        type: 'image/ico',
+                    },
+                    {
+                        src: '/image/inactive-icon.ico',
+                        type: 'image/ico',
+                    },
+                ],
+            },
+        })
+    ],
 })
